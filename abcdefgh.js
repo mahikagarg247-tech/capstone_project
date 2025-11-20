0590ed696fb9fa7546be3dcbd1a9c1fbe0ecfc81
Mahika Garg <mahikagarg@Mahikas-MacBook-Pro.local>
2025-11-20 14:57:25 +0530

    Initial commit

---
+++ abcdefgh.js
@@ -0,0 +1,24 @@
+// console.log("M")
+// console.log("G")
+
+// var a = 5;
+// console.log(a) //5
+// a = 10
+// console.log(a) //10
+
+
+// var a = 15
+
+// console.log(a) //15
+
+let a = 5;
+console.log(a) //5
+a = 10
+console.log(a) //10
+
+
+// var a = 15
+
+// console.log(a) //15
+
+
